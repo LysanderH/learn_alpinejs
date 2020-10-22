d29e99dd9ebe1512d74096a051808a922c63f904
Lysander <lysander.hans@hotmail.com>
2020-10-22 10:35:05 +0200

    ♻ Refactored > moved addTodo to the app.js file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import 'alpinejs'
 
-window.data = function (){
+window.data = function () {
 	return {
 		todos: [],
+		newTodoTitle: null,
+		addTodo() {
+			this.todos.push({
+				id: Date.now(),
+				title: this.newTodoTitle,
+				completed: false,
+			})
+		}
 	}
 }
