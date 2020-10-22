45e880d28ec98da167c4c65a282cf99505483dc0
Lysander <lysander.hans@hotmail.com>
2020-10-22 16:39:52 +0200

    🚩 Added toggle -> all todos active or completed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ window.data = function () {
 		newTodoTitle: '',
 		filter: 'all',
 		todoEditing: null,
+		checked: false,
 
 		/**
 		 * Filters todos to get not accomplished todos
@@ -15,18 +16,34 @@ window.data = function () {
 			return this.todos.filter(item => !item.completed);
 		},
 
+		/**
+		 * Counts the active todos
+		 * @returns int activeTodosCount
+		 */
 		get activeTodosCount() {
 			return this.activeTodos.length;
 		},
 
+		/**
+		 * Filters todos array to get completed todos
+		 * @returns array completedTodos
+		 */
 		get completedTodos() {
 			return this.todos.filter(item => item.completed);
 		},
 
+		/**
+		 * Counts completed todos
+		 * @returns {*}
+		 */
 		get completedTodosCount() {
 			return this.completedTodos.length;
 		},
 
+		/**
+		 * Get the filtered todos from todos array
+		 * @returns array
+		 */
 		get filteredTodos() {
 			return {
 				all: this.todos,
@@ -35,6 +52,17 @@ window.data = function () {
 			}[this.filter]
 		},
 
+		/**
+		 * Checks if all todos are completed or not
+		 * @returns {boolean}
+		 */
+		get allCompleted() {
+			return this.todos.length === this.completedTodosCount;
+		},
+
+		/**
+		 * Add an item to the todos array
+		 */
 		addTodo() {
 			if (this.newTodoTitle.trim()) {
 				this.todos.push({
@@ -46,14 +74,28 @@ window.data = function () {
 			}
 		},
 
+		/**
+		 * Removes an item from todos array
+		 * @param todo
+		 */
 		removeTodo(todo) {
 			this.todos.splice(this.todos.indexOf(todo), 1);
 		},
 
+		/**
+		 * Changes the completed value of a todos array item
+		 * @param todo
+		 */
 		toggleCompleted(todo) {
 			todo.completed = !todo.completed;
 		},
 
+		/**
+		 * Initiate the editing process of one item
+		 * @param todo
+		 * @param tick
+		 * @param field
+		 */
 		startEditing(todo, tick, field) {
 			todo.cachedTitle = todo.title;
 
@@ -68,6 +110,10 @@ window.data = function () {
 			}));
 		},
 
+		/**
+		 * Validates the updated values of the modified item
+		 * @param todo
+		 */
 		validateEditingTodo(todo) {
 			if (todo.title.trim()) {
 				delete this.todoEditing.editing;
@@ -76,10 +122,22 @@ window.data = function () {
 			}
 		},
 
-		cancelEditingTodo(todo){
+		/**
+		 * If push esc cancel the editing and reset the title
+		 * @param todo
+		 */
+		cancelEditingTodo(todo) {
 			todo.title = todo.cachedTitle;
 			delete todo.cachedTitle;
 			delete todo.editing;
+		},
+
+		/**
+		 * Toggle between completed or active if you click on mark all button
+		 */
+		toggleChecked() {
+			let allCompleted = this.allCompleted;
+			this.todos.forEach(todo => todo.completed = !allCompleted);
 		}
 
 	}
