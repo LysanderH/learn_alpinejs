8ab0183f100a9a5b49776a3295b84c85e8fcaed7
Lysander <lysander.hans@hotmail.com>
2020-10-22 11:47:50 +0200

    🚩 Applied class completed on todo list-item

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ window.data = function () {
 		},
 		removeTodo(todo) {
 			this.todos.splice(this.todos.indexOf(todo), 1);
+		},
+		toggleCompleted(todo) {
+			todo.completed = !todo.completed;
 		}
 	}
 }
