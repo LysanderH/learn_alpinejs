c0a6d6708676cd7e112e4a968dfa07b2f35baead
Lysander <lysander.hans@hotmail.com>
2020-10-22 15:17:42 +0200

    🚩 Almost added update functionality

--- src/app.js
+++ src/app.js
@@ -5,25 +5,36 @@ window.data = function () {
 		todos: [],
 		newTodoTitle: '',
 		filter: 'all',
+		todoEditing: null,
+
+		/**
+		 * Filters todos to get not accomplished todos
+		 * @returns array activeTodos
+		 */
 		get activeTodos() {
 			return this.todos.filter(item => !item.completed);
 		},
+
 		get activeTodosCount() {
 			return this.activeTodos.length;
 		},
+
 		get completedTodos() {
 			return this.todos.filter(item => item.completed);
 		},
+
 		get completedTodosCount() {
 			return this.completedTodos.length;
 		},
+
 		get filteredTodos() {
 			return {
-				all:this.todos,
+				all: this.todos,
 				active: this.activeTodos,
 				completed: this.completedTodos
 			}[this.filter]
 		},
+
 		addTodo() {
 			if (this.newTodoTitle.trim()) {
 				this.todos.push({
@@ -34,12 +45,42 @@ window.data = function () {
 				this.newTodoTitle = ''
 			}
 		},
+
 		removeTodo(todo) {
 			this.todos.splice(this.todos.indexOf(todo), 1);
 		},
+
 		toggleCompleted(todo) {
 			todo.completed = !todo.completed;
 		},
+
+		startEditing(todo, tick, field) {
+			todo.cachedTitle = todo.title;
+
+			if (this.todoEditing) {
+				delete this.todoEditing.editing;
+			}
+			this.todoEditing = todo;
+			todo.editing = true;
+
+			tick(setTimeout(() => {
+				field.focus()
+			}));
+		},
+
+		validateEditingTodo(todo) {
+			if (todo.title.trim()) {
+				delete this.todoEditing.editing;
+			} else {
+				this.removeTodo(todo);
+			}
+		},
+
+		cancelEditingTodo(todo){
+			todo.title = todo.cachedTitle;
+			delete todo.cachedTitle;
+			delete todo.editing;
+		}
 
 	}
 }
